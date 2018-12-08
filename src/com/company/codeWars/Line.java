f3c63141efa73f7ec7276296bf80668d50c451f1
MikayelyanVahe <vahe.mikayelyan80@gmail.com>
2018-12-08 15:38:03 +0400

    added 2 exercise

---
+++ src/com/company/codeWars/Line.java
@@ -0,0 +1,50 @@
+package com.company.codeWars;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Line {
+
+  public static String Tickets(int[] peopleInLine)
+    {
+        int bankNote25=0;
+        int bankNote50=0;
+        String answerYES = "YES";
+        String answerNO = "NO";
+        int arrayLenght=peopleInLine.length;
+        int i;
+
+       for (i=0; i <= peopleInLine.length-1; i++) {
+           if (peopleInLine[i] == 25) {
+               bankNote25 += 1;
+           }else
+
+           if (peopleInLine[i] == 50 && bankNote25 > 0) {
+               bankNote25 -= 1;
+               bankNote50 += 1;
+           }else
+
+           if (peopleInLine[i] == 100 && bankNote25 > 2) {
+               bankNote25 -= 3;
+
+           }else
+           if (peopleInLine[i] == 100 && bankNote50 > 0 && bankNote25 > 0) {
+               bankNote25 -= 1;
+               bankNote50 -= 1;
+           }else {
+               i = arrayLenght;
+               return answerNO;
+           }
+       }
+       return answerYES;
+    }
+
+    public static void main(String[] args) {
+        int[] lineTickets = new int[]{25,25,50,100,25,50,25,100,25,50,25,100,100,50};
+        Line line = new Line();
+        System.out.println(line.Tickets(lineTickets));
+
+//        assertEquals("YES", Line.Tickets(new int[] {25, 25, 50}));
+//        assertEquals("YES", Line.Tickets(new int[] {25, 25, 50}));
+    }
+}
