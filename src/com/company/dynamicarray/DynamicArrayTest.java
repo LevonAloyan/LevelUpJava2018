149cf8ed8215feb4e1138742aca6b380b4c51e01
MikayelyanVahe <vahe.mikayelyan80@gmail.com>
2018-12-15 09:32:25 +0400

    dynamic array

---
+++ src/com/company/dynamicarray/DynamicArrayTest.java
@@ -0,0 +1,13 @@
+package com.company.dynamicarray;
+
+public class DynamicArrayTest {
+    public static void main(String[] args) {
+        DynamicArray dy = new DynamicArrayImpl(5);
+//        dy.set(1,10);
+//        dy.set(2,15);
+//        dy.set(3,20);
+
+        System.out.println(dy.contains(102));
+
+    }
+}
