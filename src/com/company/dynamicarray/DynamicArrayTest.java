907f27b312e4a6b875619b3b4065ec8a3f19d9f6
MikayelyanVahe <vahe.mikayelyan80@gmail.com>
2018-12-15 09:32:25 +0400

    dynamic array

--- src/com/company/dynamicarray/DynamicArrayTest.java
+++ src/com/company/dynamicarray/DynamicArrayTest.java
@@ -1,28 +1,13 @@
 package com.company.dynamicarray;
 
 public class DynamicArrayTest {
-
     public static void main(String[] args) {
-        DynamicArrayImpl dynamicArray = new DynamicArrayImpl();
+        DynamicArray dy = new DynamicArrayImpl(5);
+//        dy.set(1,10);
+//        dy.set(2,15);
+//        dy.set(3,20);
 
+        System.out.println(dy.contains(102));
 
-        dynamicArray.add(5);
-        dynamicArray.add(6);
-        dynamicArray.add(4, 6);
-        dynamicArray.add(4, 9);
-        dynamicArray.add(3, 7);
-        dynamicArray.add(5, 8);
-        dynamicArray.add(6, 7);
-        dynamicArray.add(8, 6);
-        dynamicArray.add(9, 6);
-
-//        dynamicArray.removeByIndex(4);
-//        System.out.println(dynamicArray.size());
-//        System.out.println(dynamicArray.contains(6));
-//        System.out.println(dynamicArray.get(5));
-//        System.out.println(dynamicArray.remove(6));
-//        System.out.println(dynamicArray.indexOf(10));
-//        System.out.println(dynamicArray.lastIndexOf(7));
-        System.out.println(dynamicArray.set(5, 3));
     }
 }
