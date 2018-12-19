bdce20510ad59ad7af69c80bf7f732318584d9c9
MikayelyanVahe <vahe.mikayelyan80@gmail.com>
2018-12-19 14:40:41 +0400

    builder pattern example added

--- src/com/company/dynamicarray/vahe/DynamicArrayTest.java
+++ src/com/company/dynamicarray/vahe/DynamicArrayTest.java
@@ -1,33 +1,24 @@
 package com.company.dynamicarray.vahe;
 
 public class DynamicArrayTest {
+
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-        DynamicArray dy = new DynamicArrayImpl(3);
-        dy.set(0,10);
-        dy.set(1,20);
-        dy.set(2,25);
-=======
+
         DynamicArray dy = new DynamicArrayImpl(5);
         dy.set(0,10);
         dy.set(1,20);
         dy.set(2,25);
         dy.set(3,30);
         dy.set(4,35);
->>>>>>> Stashed changes
-
 
 //        System.out.println(dy.contains(15));
 //        System.out.println(dy.get(2));
 //        System.out.println(dy.lastIndexOf(20));
 //        System.out.println(dy.indexOf(10));
-<<<<<<< Updated upstream
+
           dy.add(1,45);
           dy.add(2,50);
-=======
+
         System.out.println(dy.remove(50));
-
->>>>>>> Stashed changes
-
     }
 }
