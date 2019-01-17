63cde509a8024871dd08385b4bea33e270cb0c31
Aloyan, Levon (EPAM System Inc.) <Levon.Aloyan@ihg.com>
2019-01-17 12:18:48 +0400

    add wildcards examples and comparator

--- src/com/company/algorithms/Test.java
+++ src/com/company/algorithms/Test.java
@@ -1,9 +1,30 @@
 package com.company.algorithms;
 
 import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
+        User levon = new User("Levon",24);
+        User gagik = new User("Gagik",22);
+        User vahe = new User("Vahe",38);
+        User aram = new User("Aram",20);
+        User ashot = new User("Ashot",28);
+        User ash= new User("Ash",21);
+
+        Set<User> users = new TreeSet<>();
+        users.add(levon);
+        users.add(gagik);
+        users.add(vahe);
+        users.add(aram);
+        users.add(ashot);
+        users.add(ash);
+
+        for (User user : users){
+            System.out.println(user);
+        }
+
 
     }
 
