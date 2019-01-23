b1f2eba5589ff34508ba990e07ec9c0fbc1e829b
MikayelyanVahe <vahe.mikayelyan80@gmail.com>
2019-01-23 13:17:38 +0400

    Student compare

--- src/com/company/Generics/Student/StudentComparator.java
+++
@@ -1,9 +0,0 @@
-package com.company.Generics.Student;
-import java.util.Comparator;
-
-public class StudentComparator implements Comparator<String> {
-    @Override
-    public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
-    }
-}
